8d12266564a0eaac930e259ea7e575b02fef53bc
maximus-xu <maximusxu32@gmail.com>
2020-12-06 14:32:15 -0800

    Create a code that estimates pi

---
+++ EstimatePi.py
@@ -0,0 +1,22 @@
+import random
+repeat_int = 0
+
+
+def generate_random():
+    return random.random()
+
+
+inside = 0
+total = 1000000
+
+while repeat_int <= total:
+    x = generate_random()
+    y = generate_random()
+    if x*x + y*y <= 1:
+        inside += 1
+    repeat_int += 1
+estimate = (inside/total) * 4
+print(estimate)
+pi = 3.141592653589793238462
+
+print("{:1.2f}%".format((estimate-pi)/pi*100))
