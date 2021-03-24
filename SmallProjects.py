e11835b600d84f0c78e4c93db2e7b8530b5b7414
maximus-xu <75446801+maximus-xu@users.noreply.github.com>
2021-03-24 13:20:19 -0700

    Pasted "count" function into SmallProjects.py

--- SmallProjects.py
+++ SmallProjects.py
@@ -11,3 +11,20 @@ def parenthesis(left, right, input):
     else:
         parenthesis(left - 1, right, input + '(')
         parenthesis(left, right - 1, input + ')')
+
+
+def count(input, search_number):
+    if len(input) == 0:
+        return 0
+    elif input[0] == search_number and input[-1] == search_number:
+        return len(input)
+    elif input[0] > search_number:
+        return 0
+    elif input[-1] < search_number:
+        return 0
+
+    middle = round(len(input ) /2)
+    return count(input[:middle], search_number) + count(input[middle:], search_number)
+
+
+print(count([1], 1))
