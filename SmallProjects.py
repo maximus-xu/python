73594eadf799d6ddc70058ec32bec5140fe0da3d
maximus-xu <75446801+maximus-xu@users.noreply.github.com>
2021-03-24 13:54:39 -0700

    Added "number_combo" function to SmallProjects.py

--- SmallProjects.py
+++ SmallProjects.py
@@ -12,3 +12,22 @@ def parenthesis(left, right, input):
         parenthesis(left - 1, right, input + '(')
         parenthesis(left, right - 1, input + ')')
 
+
+def number_combo(input, output):
+    if not input:
+        total = 0
+        for i in output:
+            total += i
+        if total == 100:
+            print(output)
+        return
+
+    short = input[1:]
+
+    if not output:
+        number_combo(short, [input[0]])
+        number_combo(short, [-input[0]])
+    else:
+        number_combo(short, output + [input[0]])
+        number_combo(short, output + [-input[0]])
+        number_combo(short, output[:-1] + [output[-1] * 10 + input[0]] if output[-1] > 0 else [output[-1] * 10] + [-input[0]])
