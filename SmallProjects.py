f4214ee72a0bde9f654ff504553ba7fdffcac4df
maximus-xu <75446801+maximus-xu@users.noreply.github.com>
2021-03-25 09:53:34 -0700

    (FINALLY) Finished 'string_equation' function

--- SmallProjects.py
+++ SmallProjects.py
@@ -30,6 +30,36 @@ def number_combo(input, output):
         number_combo(short, output + [input[0]])
         number_combo(short, output + [-input[0]])
         number_combo(short, output[:-1] + [output[-1] * 10 + input[0]] if output[-1] > 0 else
-                                                                                [output[-1] * 10] + [-input[0]])
+        [output[-1] * 10] + [-input[0]])
 
 
+def string_equation(input):
+    equation = ''
+    for i in range(len(input)):
+        if input[i] != ' ':
+            equation += input[i]
+    equation = equation.split('+')
+    if type(equation) == str:
+        equation = [equation]
+    total = 0
+    for i in range(len(equation)):
+        try:
+            equation[i] = int(equation[i])
+        except:
+            sub_total = 0
+            equation[i] = equation[i].split('-')
+
+            for j in range(len(equation[i])):
+                if j == 0:
+                    sub_total += int(equation[i][j])
+                else:
+                    sub_total -= int(equation[i][j])
+            equation[i] = sub_total
+
+    for i in equation:
+        total += i
+
+    print(total)
+
+
+string_equation('')
