5d659ea61299376b8d48286f7357447d14b91e03
maximus-xu <75446801+maximus-xu@users.noreply.github.com>
2021-03-28 10:43:27 -0700

    Added string_equation_2 and get_first_number functions

--- SmallProjects.py
+++ SmallProjects.py
@@ -34,12 +34,11 @@ def number_combo(input, output):
 
 
 def string_equation(input):
-    equation = ''
+    equation = []
 
     for i in range(len(input)):
         if input[i] != ' ':
-            equation += input[i]
-    equation = equation.split('+')
+            equation += [input[i]]
     total = 0
 
     for i in range(len(equation)):
@@ -55,3 +54,37 @@ def string_equation(input):
         total += i
 
     print(total)
+
+
+def string_equation_2(input):
+    total = 0
+    i = 0
+    while i < len(input):
+        first, index = get_first_number(input[i:])
+        total += first
+        i += index
+    return total
+
+
+def get_first_number(input):
+    digits = [c for c in "0123456789"]
+    negative = 0
+    number = 0
+    for i in range(len(input)):
+        if input[i] == '-':
+            negative += 1
+        elif input[i] in digits:
+            index = i
+            break
+
+    for i in range(index, len(input)):
+        if input[i] in digits:
+            number *= 10
+            number += int(input[i])
+        else:
+            break
+
+    if negative % 2 == 0:
+        return number, i+1 if i == len(input)-1 else i
+    return -number, i+1 if i == len(input)-1 else i
+
