0970031af8e2a45fbab278d7f157ebb05655cd81
maximus-xu <75446801+maximus-xu@users.noreply.github.com>
2021-04-02 15:36:13 -0700

    Misc updates

--- SmallProjects.py
+++ SmallProjects.py
@@ -57,36 +57,35 @@ def string_equation(input):
 
 
 def string_equation_2(input):
+    def get_first_number(input):
+        digits = [c for c in "0123456789"]
+        negative = 0
+        number = 0
+        for i in range(len(input)):
+            if input[i] == '-':
+                negative += 1
+            elif input[i] in digits:
+                index = i
+                break
+
+        for i in range(index, len(input)):
+            if input[i] in digits:
+                number *= 10
+                number += int(input[i])
+            else:
+                break
+
+        if negative % 2 == 0:
+            return number, i + 1 if i == len(input) - 1 else i
+        return -number, i + 1 if i == len(input) - 1 else i
+
     total = 0
     i = 0
     while i < len(input):
         first, index = get_first_number(input[i:])
         total += first
         i += index
-    return total
-
-
-def get_first_number(input):
-    digits = [c for c in "0123456789"]
-    negative = 0
-    number = 0
-    for i in range(len(input)):
-        if input[i] == '-':
-            negative += 1
-        elif input[i] in digits:
-            index = i
-            break
-
-    for i in range(index, len(input)):
-        if input[i] in digits:
-            number *= 10
-            number += int(input[i])
-        else:
-            break
-
-    if negative % 2 == 0:
-        return number, i+1 if i == len(input)-1 else i
-    return -number, i+1 if i == len(input)-1 else i
+    print(total)
 
 
 def eight_queens(positions):
@@ -119,5 +118,3 @@ def eight_queens(positions):
     for i in possible:
         eight_queens(positions + [[row, i]])
 
-
-eight_queens([])
